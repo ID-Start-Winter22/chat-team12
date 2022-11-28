6a90779c26f79014cd3a66a4b22977850f8ce9a0
Suhal Enayat <suhal.enayat@gmail.com>
2022-11-28 23:40:17 +0100

    Fixed and extended actions

--- Botto/actions/actions.py
+++ Botto/actions/actions.py
@@ -4,28 +4,6 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
 from sys import displayhook
 from typing import Any, Text, Dict, List
 from pyparsing import nestedExpr
@@ -33,17 +11,7 @@ from pyparsing import nestedExpr
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-
-class ActionStoreUserName(Action):
-
-     def name(self) -> Text:
-         return "action_store_name"
-         
-     def run(self, dispatcher, tracker, domain):
-        username = tracker.get_slot("username")
-        print("Sender ID: ", tracker.sender_id)
-
-        return []
+from typing import Any, Text, Dict, List
 
 
 class ActionUserName(Action):
@@ -57,5 +25,23 @@ class ActionUserName(Action):
             dispatcher.utter_message(" Du hast mir Deinen Namen nicht gesagt.")
         else:
             dispatcher.utter_message(' Du bist {}'.format(username))
+        
+        print("Sender ID: ", tracker.sender_id)
+        return []
+
+class ActionHAskWithName(Action):
+
+    def name(self) -> Text:
+        return "action_ask_with_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        username = tracker.get_slot("username")
+        if not username :
+            dispatcher.utter_message("Hallo! Wie geht es dir?")
+        else:
+            dispatcher.utter_message(text="Hallo " + username + ", wie geht es dir?")
 
         return []
